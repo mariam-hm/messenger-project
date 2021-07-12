406791b8eb42313723895aeb4b30c8afa103211f
Mariam Hammoud <hamm.mariam@gmail.com>
2021-07-12 11:45:47 -0400

    Setup the project and connected to local database

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+//   logging: false
+// });
+
+const db = new Sequelize('messenger', 'postgres', 'oD!48xmY1$CL', {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
 module.exports = db;
