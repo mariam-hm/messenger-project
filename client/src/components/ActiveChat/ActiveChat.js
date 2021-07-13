f798d009e762bfd7616221790785b6dc648c4bbd
Mariam Hammoud <hamm.mariam@gmail.com>
2021-07-13 15:30:10 -0400

    Adding reversing in ActiveChat (forgot to let it from ticket 01)

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { Input, Header, Messages } from "./index";
 import { connect } from "react-redux";
+import moment from 'moment';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -35,7 +36,7 @@ const ActiveChat = (props) => {
           />
           <Box className={classes.chatContainer}>
             <Messages
-              messages={conversation.messages}
+              messages={conversation.messages.sort((a, b) => { return moment(a.createdAt).diff(b.createdAt); })}
               otherUser={conversation.otherUser}
               userId={user.id}
             />
